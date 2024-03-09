e2ff6416b135cf23917a051564eaaab9082e60e6
JackyWay <jackybbc@gmail.com>
2024-03-09 14:52:42 +0800

    version 1.0.0

--- main.tf
+++ main.tf
@@ -9,19 +9,11 @@ terraform {
   }
 }
 
-locals {
-  tags = {
-    "Environment" = var.environment
-  }
-}
-
 resource "azurerm_storage_account" "securestorage" {
   resource_group_name           = var.resource_group_name
   location                      = var.location
-  name                          = var.name
-  account_tier                  = "Standard"
-  account_replication_type      = var.environment == "Production" ? "GRS" : "LRS"
+  name                          = var.storage_account_name
+  account_tier                  = var.account_tier
+  account_replication_type      = var.account_replication_type
   public_network_access_enabled = false
-  tags                          = local.tags
-
 }
--- variables.tf
+++ variables.tf
@@ -6,10 +6,19 @@ variable "location" {
   type = string
 }
 
-variable "name" {
+variable "storage_account_name" {
   type = string
 }
 
-variable "environment" {
-  type = string
+variable "account_tier" {
+  type        = string
+  description = "The storage account tier: Standard or Premium"
+  default     = "Standard"
+  # Note about validation
+}
+
+variable "account_replication_type" {
+  type        = string
+  description = "The storage account replication type: LRS, GRS, RAGRS, ZRS, GZRS, RAGRS"
+  default     = "GRS"
 }
