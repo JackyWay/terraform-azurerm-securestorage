fd99453cfef3a26c50382ae4b186afb655cc005b
JackyWay <jackybbc@gmail.com>
2024-03-09 15:02:28 +0800

    Version 1.0.1

--- main.tf
+++ main.tf
@@ -1,6 +1,5 @@
 terraform {
   required_version = ">=1.3.0"
-
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
@@ -12,8 +11,8 @@ terraform {
 resource "azurerm_storage_account" "securestorage" {
   resource_group_name           = var.resource_group_name
   location                      = var.location
-  name                          = var.name
-  account_tier                  = "Standard"
-  account_replication_type      = var.environment == "Production" ? "GRS" : "LRS"
+  name                          = var.storage_account_name
+  account_tier                  = var.account_tier
+  account_replication_type      = var.account_replication_type
   public_network_access_enabled = false
 }
